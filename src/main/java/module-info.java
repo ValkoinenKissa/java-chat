c85e289cd6eb896392a594a1b11bef341b30137b
Alberto Bollo <albertobollo7@protonmail.com>
2026-04-23 11:34:11 +0200

    Updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module me.abollo.javachat {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens me.abollo.javachat to javafx.fxml;
     exports me.abollo.javachat;
